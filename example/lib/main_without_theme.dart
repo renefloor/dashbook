21e27b8524c3d8e5f3200c8218d0917f4373b68b
vikrech <viktor.krechetov@ah.nl>
2023-07-05 13:19:32 +0300

    Fixed example issue

--- example/lib/main_without_theme.dart
+++ example/lib/main_without_theme.dart
@@ -1,13 +1,27 @@
 import 'package:dashbook/dashbook.dart';
+import 'package:example/main.dart';
 import 'package:example/stories.dart';
 import 'package:example/text_story.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  final dashbook = Dashbook(autoPinStoriesOnLargeScreen: true);
+  final dashbook = MultiBrandBookBuilder(
+    brands: [
+      DashbookBrand(
+        name: 'love',
+        themeSettings: getThemes(TestBrand.love),
+        icon: const Icon(Icons.favorite),
+      ),
+      DashbookBrand(
+        name: 'hate',
+        themeSettings: getThemes(TestBrand.hate),
+        icon: const Icon(Icons.heart_broken_outlined),
+      ),
+    ],
+  );
 
   addTextStories(dashbook);
   addStories(dashbook);
 
-  runApp(dashbook);
+  runApp(dashbook.build());
 }
